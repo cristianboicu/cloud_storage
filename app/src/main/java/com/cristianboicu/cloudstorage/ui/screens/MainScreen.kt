8db54b99dff2532ef9dc01d9618b1f5675745007
Cristian <67103037+cristianboicu@users.noreply.github.com>
2024-01-13 17:28:50 +0200

    Gradle and min sdk update (#2)
    Co-authored-by: Cristian Boicu <cristian.boicu@tricubiq.com>

--- app/src/main/java/com/cristianboicu/cloudstorage/ui/screens/MainScreen.kt
+++ app/src/main/java/com/cristianboicu/cloudstorage/ui/screens/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.cristianboicu.cloudstorage.ui.screens
 
+import android.annotation.SuppressLint
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.*
@@ -8,6 +9,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
